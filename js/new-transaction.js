1db7b70d347dafd0a85e3281465337937f4e0512
kimseongtae <rlatjdxo4907@naver.com>
2021-09-03 17:12:32 +0900

    JS : 새로운 거래

--- js/new-transaction.js
+++ js/new-transaction.js
@@ -9,12 +9,6 @@ const $expenditure = document.querySelector('.expenditure');
 const $content = document.querySelector('.content');
 const $price = document.querySelector('.price');
 
-const year = $year.value;
-const month = $month.value;
-const date = $dates.value;
-const content = $content.value;
-const pirce = $price.value;
-let incomeExpenditure = '';
 
 $incomeExpenditure.addEventListener('click',(event) => {
   if(event.target === $income) {
@@ -28,8 +22,47 @@ $incomeExpenditure.addEventListener('click',(event) => {
   };
 })
 
+function upDateList (transactionList) {
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  div.className = 'divContainer';
+  const divFirstChild = document.createElement('div');
+  divFirstChild.className = 'divFirstChild';
+  const divSecondChild = document.createElement('div');
+  divSecondChild.className = 'divSecondChild';
+  const span1 = document.createElement('span');
+  const span2 = document.createElement('span');
+  const span3 = document.createElement('span');
+  const span4 = document.createElement('span');
+  span1.innerText = `${transactionList.year}-${transactionList.month}-${transactionList.date}`;
+  span2.innerText = incomeExpenditure;
+  span3.innerText = transactionList.content;
+  span4.innerText = `${transactionList.price} 원`;
+  $list.append(li);
+  li.append(div);
+  div.append(divFirstChild);
+  div.append(divSecondChild);
+  divFirstChild.append(span1);
+  divFirstChild.append(span2);
+  divSecondChild.append(span3);
+  divSecondChild.append(span4);
+  if(incomeExpenditure === '수입') {
+    div.style.backgroundColor = 'rgba(0, 0, 255, 0.5)';
+  } else if (incomeExpenditure === '지출') {
+    div.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+  }
+}
+
 function addTransaction(event) {
   event.preventDefault();
+  const transactionList = {
+    year : $year.value,
+    month : $month.value,
+    date : $dates.value,
+    content : $content.value,
+    price : $price.value  
+  }
+  upDateList(transactionList);
 }
 
 $form.addEventListener('submit',addTransaction);
