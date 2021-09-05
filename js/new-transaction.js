ee60dd2edae8f66b141cfac6d6daf03e4d12b247
kimseongtae <rlatjdxo4907@naver.com>
2021-09-06 03:00:27 +0900

    JS : 리스트 추가 삭제기능

--- js/new-transaction.js
+++ js/new-transaction.js
@@ -11,6 +11,49 @@ const $price = document.querySelector('.price');
 
 const list = [];
 
+function upDateList(transactionList) {
+  const li = document.createElement('li');
+  const divContainer = document.createElement('div');
+  const divFirstChild = document.createElement('div');
+  const divSecondChild = document.createElement('div');
+  const spanDay = document.createElement('span');
+  const spanIncomeExpenditure = document.createElement('span');
+  const spanContent = document.createElement('span');
+  const spanPrice = document.createElement('span');
+  const deleteBtn = document.createElement('button');
+
+  $list.appendChild(li);
+  li.appendChild(divContainer);
+  divContainer.appendChild(divFirstChild);
+  divContainer.appendChild(divSecondChild);
+  divFirstChild.appendChild(spanDay);
+  divFirstChild.appendChild(spanIncomeExpenditure);
+  divSecondChild.appendChild(spanContent);
+  divSecondChild.appendChild(spanPrice);
+
+  divContainer.className = 'divContainer';
+  divFirstChild.className = 'divFirstChild';
+  divSecondChild.className = 'divSecondChild';
+
+  deleteBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+  spanDay.innerText = `${transactionList.year} - ${transactionList.month} - ${transactionList.date}`;
+  spanIncomeExpenditure.innerText = `${transactionList.incomeExpenditure}`;
+  spanContent.innerText = `${transactionList.content}`;
+  spanPrice.innerText = `${transactionList.price} 원 `;
+  
+  spanPrice.appendChild(deleteBtn);
+
+  if(incomeExpenditure === '수입') {
+    divContainer.style.backgroundColor = 'rgba(0, 0, 255, 0.5)';
+  } else if (incomeExpenditure === '지출') {
+    divContainer.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+  }
+
+  deleteBtn.addEventListener('click', (event) => {
+    event.target.parentNode.parentNode.parentNode.parentNode.remove();
+  });
+}
+
 $incomeExpenditure.addEventListener('click',(event) => {
   if(event.target === $income) {
     $income.style.backgroundColor = '#ffd400';
@@ -34,6 +77,7 @@ function addTransaction(event) {
     incomeExpenditure : incomeExpenditure
   }
   list.push(transactionList);
+  upDateList(transactionList);
   $income.style.backgroundColor = '';
   $expenditure.style.backgroundColor = '';
   $year.value = '';
@@ -45,3 +89,23 @@ function addTransaction(event) {
 
 $form.addEventListener('submit',addTransaction);
 
+const $history__title = document.querySelector('.transaction-history__title');
+const $history__all = document.querySelector('.history__all');
+const $history__income = document.querySelector('.history__income');
+const $history__expenditure = document.querySelector('.history__expenditure');
+
+$history__title.addEventListener('click',(event) => {
+  if (event.target === $history__income) {
+    $history__all.style.color = 'rgba(0,0,0,0.1)';
+    $history__income.style.color = 'rgba(0,0,0,1)';
+    $history__expenditure.style.color = 'rgba(0,0,0,0.1)';
+  } else if (event.target === $history__expenditure) {
+    $history__all.style.color = 'rgba(0,0,0,0.1)';
+    $history__income.style.color = 'rgba(0,0,0,0.1)';
+    $history__expenditure.style.color = 'rgba(0,0,0,1)';
+  } else if (event.target === $history__all) {
+    $history__all.style.color = 'rgba(0,0,0,1)';
+    $history__income.style.color = 'rgba(0,0,0,0.1)';
+    $history__expenditure.style.color = 'rgba(0,0,0,0.1)';
+  }
+})
